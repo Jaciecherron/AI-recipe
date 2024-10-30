d216e5ed239c5da90a10b2f57aff25a6b5b2cfa7
Jaciecherron <jaciecherron@gmail.com>
2024-10-30 17:11:37 -0600

    Added API and Axios

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+function displayRecipe (response) {
+
+    new Typewriter("#recipe", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 5,
+      curser: "",
+    });
+}
+
+
 function generateRecipe (event) {
   event.preventDefault ();
-
-  new Typewriter("#recipe", {
-    strings: `recipe`,
-    autoStart: true,
-    delay: 5,
-    curser: "",
-  });
 }
+
+let instructionsInput = document.querySelector("#instructions")
+
+let apiKey = "9f3o6449dc310bta33096fd85b205350";
+let prompt = `You are a top chef AI Assistant and can give a short and simple recipe with ingredients at the top and the instructions at the bottom with the title on a different line based off ${instructionsInput.value}`;
+let context = "generage a recipe with ingredients and instructions";
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log (`prompt: ${prompt}`);
+console.log (`context: ${context}`);
+
+
+axios.get(apiURL).then(displayRecipe);
 
 let recipeFormElement =document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener ("submit", generateRecipe);
