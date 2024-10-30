928f31c68ab535460db8fa2571881dba17e6ee1f
Jaciecherron <jaciecherron@gmail.com>
2024-10-30 17:18:30 -0600

    moved api information

--- index.js
+++ index.js
@@ -11,20 +11,17 @@ function displayRecipe (response) {
 
 function generateRecipe (event) {
   event.preventDefault ();
-}
 
-let instructionsInput = document.querySelector("#instructions")
+let instructionsInput = document.querySelector("#instructions");
 
 let apiKey = "9f3o6449dc310bta33096fd85b205350";
 let prompt = `You are a top chef AI Assistant and can give a short and simple recipe with ingredients at the top and the instructions at the bottom with the title on a different line based off ${instructionsInput.value}`;
 let context = "generage a recipe with ingredients and instructions";
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log (`prompt: ${prompt}`);
-console.log (`context: ${context}`);
-
-
 axios.get(apiURL).then(displayRecipe);
+}
+
 
 let recipeFormElement =document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener ("submit", generateRecipe);
